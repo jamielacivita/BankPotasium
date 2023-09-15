a96a51bfb523b1f900305002054505fcd35dd3fe
Jamie Lacivita <JCLacivita@gmail.com>
2023-09-15 09:59:02 -0600

    Added logging.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-
+import logging.config
+logging.config.fileConfig("logging_config.ini")
+log = logging.getLogger(__name__)
 
 def main():
+    log.debug("Running main function")
     print("JWTO")
 
 
