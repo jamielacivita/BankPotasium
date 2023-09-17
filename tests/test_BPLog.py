ac2e28e050f2c3139b3e6f95b79025ae35f67954
vedranson <vedranson@gmail.com>
2023-09-17 12:12:29 +0200

    rename Measurement to Datapoint, general code readability

--- tests/test_BPLog.py
+++ tests/test_BPLog.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 
 from src import BPLog as BP
-from src.BPLog import Measurement
+from src.BPLog import Datapoint
 
 
 @pytest.fixture
@@ -14,17 +14,17 @@ def bplog():
 
 
 def test_add_measurement_date(bplog):
-    bplog.add_measurement(Measurement(date="230915", sys="120", dia="80"))
+    bplog.add_measurement(Datapoint(date="230915", sys="120", dia="80"))
     assert bplog.measurements[0].date == "230915"
 
 
 def test_add_measurement_sys(bplog):
-    bplog.add_measurement(Measurement(date="230915", sys="120", dia="80"))
+    bplog.add_measurement(Datapoint(date="230915", sys="120", dia="80"))
     assert bplog.measurements[0].sys == "120"
 
 
 def test_add_measurement_dia(bplog):
-    bplog.add_measurement(Measurement(date="230915", sys="120", dia="80"))
+    bplog.add_measurement(Datapoint(date="230915", sys="120", dia="80"))
     assert bplog.measurements[0].dia == "80"
 
 
@@ -32,88 +32,88 @@ def test_get_unique_dates(bplog):
     expected = []
     assert bplog._get_unique_dates() == expected
 
-    bplog.add_measurement(Measurement(date="230915", sys="120", dia="80"))
+    bplog.add_measurement(Datapoint(date="230915", sys="120", dia="80"))
     expected = ["230915"]
     assert bplog._get_unique_dates() == expected
 
     expected = ["230915", "230916"]
-    bplog.add_measurement(Measurement(date="230916", sys="120", dia="80"))
+    bplog.add_measurement(Datapoint(date="230916", sys="120", dia="80"))
     assert bplog._get_unique_dates() == expected
-    bplog.add_measurement(Measurement(date="230916", sys="120", dia="80"))
+    bplog.add_measurement(Datapoint(date="230916", sys="120", dia="80"))
     assert bplog._get_unique_dates() == expected
 
 
 def test_get_daily_measurements(bplog):
-    bplog.add_measurement(Measurement(date="230915", sys="1", dia="1"))
-    bplog.add_measurement(Measurement(date="230915", sys="2", dia="2"))
-    bplog.add_measurement(Measurement(date="230916", sys="3", dia="3"))
+    bplog.add_measurement(Datapoint(date="230915", sys="1", dia="1"))
+    bplog.add_measurement(Datapoint(date="230915", sys="2", dia="2"))
+    bplog.add_measurement(Datapoint(date="230916", sys="3", dia="3"))
 
     date = "230915"
     expected = [
-        Measurement(date="230915", sys="1", dia="1"),
-        Measurement(date="230915", sys="2", dia="2"),
+        Datapoint(date="230915", sys="1", dia="1"),
+        Datapoint(date="230915", sys="2", dia="2"),
     ]
     assert bplog._get_daily_measurements(date) == expected
 
     date = "230916"
     expected = [
-        Measurement(date="230916", sys="3", dia="3"),
+        Datapoint(date="230916", sys="3", dia="3"),
     ]
     assert bplog._get_daily_measurements(date) == expected
 
 
 def test_calc_averages(bplog):
     daily_measurements = [
-        Measurement(date="230915", sys="120", dia="80"),
-        Measurement(date="230915", sys="140", dia="100"),
+        Datapoint(date="230915", sys="120", dia="80"),
+        Datapoint(date="230915", sys="140", dia="100"),
     ]
     expected = 130, 90
     assert bplog._calc_averages(daily_measurements) == expected
 
 
 def test_set_measurements_daily_avg(bplog):
-    bplog.add_measurement(Measurement(date="230901", sys="1", dia="1"))
-    expected = [Measurement("230901", 1, 1)]
-    bplog.set_measurements_daily_avg()
-    assert bplog.measurements_daily_avg == expected
+    bplog.add_measurement(Datapoint(date="230901", sys="1", dia="1"))
+    expected = [Datapoint("230901", 1, 1)]
+    bplog.calc_daily_avg()
+    assert bplog.daily_avg == expected
 
-    bplog.add_measurement(Measurement(date="230901", sys="2", dia="2"))
-    expected = [Measurement("230901", 1, 1)]
-    bplog.set_measurements_daily_avg()
-    assert bplog.measurements_daily_avg == expected
+    bplog.add_measurement(Datapoint(date="230901", sys="2", dia="2"))
+    expected = [Datapoint("230901", 1, 1)]
+    bplog.calc_daily_avg()
+    assert bplog.daily_avg == expected
 
-    bplog.add_measurement(Measurement(date="230901", sys="3", dia="3"))
-    expected = [Measurement("230901", 2, 2)]
-    bplog.set_measurements_daily_avg()
-    assert bplog.measurements_daily_avg == expected
+    bplog.add_measurement(Datapoint(date="230901", sys="3", dia="3"))
+    expected = [Datapoint("230901", 2, 2)]
+    bplog.calc_daily_avg()
+    assert bplog.daily_avg == expected
 
 
 def test_set_measurements_sevenday_avg(bplog):
-    bplog.add_measurement(Measurement(date="230901", sys="1", dia="1"))
-    bplog.add_measurement(Measurement(date="230902", sys="2", dia="2"))
-    bplog.add_measurement(Measurement(date="230903", sys="3", dia="3"))
-    bplog.add_measurement(Measurement(date="230904", sys="4", dia="4"))
-    bplog.add_measurement(Measurement(date="230905", sys="5", dia="5"))
-    bplog.add_measurement(Measurement(date="230906", sys="6", dia="6"))
+    bplog.add_measurement(Datapoint(date="230901", sys="1", dia="1"))
+    bplog.add_measurement(Datapoint(date="230902", sys="2", dia="2"))
+    bplog.add_measurement(Datapoint(date="230903", sys="3", dia="3"))
+    bplog.add_measurement(Datapoint(date="230904", sys="4", dia="4"))
+    bplog.add_measurement(Datapoint(date="230905", sys="5", dia="5"))
+    bplog.add_measurement(Datapoint(date="230906", sys="6", dia="6"))
 
     expected = []
-    bplog.set_measurements_daily_avg()
-    bplog.set_measurements_sevenday_avg()
-    assert bplog.measurements_sevenday_avg == expected
+    bplog.calc_daily_avg()
+    bplog.calc_seven_day_avg()
+    assert bplog.seven_day_avg == expected
 
-    bplog.add_measurement(Measurement(date="230907", sys="7", dia="7"))
-    bplog.add_measurement(Measurement(date="230908", sys="8", dia="8"))
-    bplog.add_measurement(Measurement(date="230909", sys="9", dia="9"))
+    bplog.add_measurement(Datapoint(date="230907", sys="7", dia="7"))
+    bplog.add_measurement(Datapoint(date="230908", sys="8", dia="8"))
+    bplog.add_measurement(Datapoint(date="230909", sys="9", dia="9"))
 
     expected = [
-        Measurement(date="230907", sys=4, dia=4),
-        Measurement(date="230908", sys=5, dia=5),
-        Measurement(date="230909", sys=6, dia=6),
+        Datapoint(date="230907", sys=4, dia=4),
+        Datapoint(date="230908", sys=5, dia=5),
+        Datapoint(date="230909", sys=6, dia=6),
     ]
 
-    bplog.set_measurements_daily_avg()
-    bplog.set_measurements_sevenday_avg()
-    assert bplog.measurements_sevenday_avg == expected
+    bplog.calc_daily_avg()
+    bplog.calc_seven_day_avg()
+    assert bplog.seven_day_avg == expected
 
 
 def test_import_from_cvs(bplog):
@@ -125,8 +125,8 @@ def test_import_from_cvs(bplog):
 
     bplog.import_from_csv(test_file)
     expected = [
-        Measurement(date='1', sys='1', dia='1'),
-        Measurement(date='2', sys='2', dia='2'),
+        Datapoint(date='1', sys='1', dia='1'),
+        Datapoint(date='2', sys='2', dia='2'),
     ]
     assert bplog.measurements == expected
 
